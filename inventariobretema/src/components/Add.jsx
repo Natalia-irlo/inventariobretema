427b95deffc2d5e5f90863b6cb8ac6e06d9a0d0c
natalia <natiriarte5@gmail.com>
2023-03-14 12:54:15 +0100

    add

--- inventariobretema/src/components/Add.jsx
+++ inventariobretema/src/components/Add.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
-function Add () {
+
+function Add() {
   return (
-    <div>
-        <h1>esta es la add</h1>
-    </div>
+    <>
+      <form className='addproduct'>
+        <h2>Añadir producto</h2>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"><b>Nombre*</b></label>
+          <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"><b>Características</b></label>
+          <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"><b>Imagen*</b></label>
+          <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+        </div>
+        <button type="submit" className='boton-add'>AÑADIR</button>
+      </form>
+    </>
   )
 }
 
