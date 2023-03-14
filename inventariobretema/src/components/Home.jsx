905e976d3909b84d5009e6751e19e69bdf161a5b
natalia <natiriarte5@gmail.com>
2023-03-14 10:10:29 +0100

    home

--- inventariobretema/src/components/Home.jsx
+++ inventariobretema/src/components/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import logosinfondo from '../assets/img/logosinfondo.png';
 
 function Home () {
   return (
-    <div>
-        <h1>esta es la home</h1>
+    <div className='container-home'>
+      <div className='container-logog'>
+        <img className="logo-grande" src={logosinfondo} alt="logo" width="500px" />
+      </div>
+      <div className='container-botonh'>
+        <button className='boton-login'><p><b>LOG IN</b></p></button>
+      </div>
     </div>
   )
 }
